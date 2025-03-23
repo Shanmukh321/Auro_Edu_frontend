e8354f67abc42733c9e7f750b06b7fe5a4946da8
shanmukhsworkspace-admin <shanmukhasai_jogi@srmap.edu.in>
2025-03-23 18:09:54 +0530

    toggle

--- src/app/components/ThemeToggle.tsx
+++ src/app/components/ThemeToggle.tsx
@@ -1,12 +1,12 @@
 import { useChatStore } from '../lib/store';
 export default function ThemeToggle() {
-  const { theme, toggleTheme } = useChatStore();
+  const { theme, setTheme } = useChatStore();
 
   return (
     <button
       onClick={() => {
         console.log('Toggle clicked, theme was:', theme);
-        toggleTheme();
+        setTheme(theme);
       }}
       className="p-2 bg-gray-200 dark:bg-gray-700 dark:text-white rounded-xl transition delay-250 hover:-translate-y-1 hover:scale-110 hover:bg-gray-300 dark:hover:bg-gray-600 transition" 
     >
